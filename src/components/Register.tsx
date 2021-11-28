8643d35eb8bec92972ef4e0ab95c48b3c10ccb96
physcript <physcript@gmail.com>
2021-11-28 18:16:37 +0800

    register page

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState } from 'react';
 import { Button, FormLabel } from 'react-bootstrap';
 
@@ -5,6 +6,16 @@ import { IRegisterUserProps } from '../interface/form/user';
 
 export interface IRegisterProps {}
 const Register:React.FunctionComponent<IRegisterProps> = (props) => {
+
+    const [ loading,setLoading ] = useState(false)
+    const [ success,setSuccess ] = useState('')
+    const [ error,setError ] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
 
     const [ iReg,setIReg ] = useState<IRegisterUserProps>({
 
@@ -25,10 +36,67 @@ const Register:React.FunctionComponent<IRegisterProps> = (props) => {
         }) )
     }
 
+    const regiterHandler = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
+        setLoading(true)
+
+        axios.request({
+            url:'http://localhost:1337/api/register',
+            method: 'POST',
+            data: {
+                firstName: iReg.firstName,
+                lastName: iReg.lastName,
+                email: iReg.email,
+                password: iReg.password,
+                confirmPassword: iReg.confirmPassword
+            }
+        }).then((res) => {
+            setLoading(false)
+            setIReg({
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            })
+
+            setError({
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            })
+            setSuccess('Account Created ✔️')
+        }).catch((err) => {
+            setLoading(false)
+            setSuccess('')
+            setError(err.response.data.error)
+
+        })
+    }
+
     return (
         <form>
-            <div className = "w-50 d-flex flex-column gap-2 m-auto"   >
-                <FormLabel>Register</FormLabel>
+            { loading ? (
+                <div className = " d-flex flex-column gap-2  align-items-center ">
+                    <div className = "spinner-border d-flex align-items-center">
+                        <span className = "sr-only"></span>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <div className = "w-50 d-flex flex-column gap-2 m-auto">
+
+                    <label className = "text-success">{ success }</label>
+
+                        <label className = "text-danger">{ error?.firstName }</label>
+                    <label className = "text-danger">{ error?.lastName }</label>
+                    <label className = "text-danger">{ error?.email }</label>
+                    <label className = "text-danger">{ error?.password }</label>
+                    <label className = "text-danger">{ error?.confirmPassword }</label>
+
+                    <FormLabel>Register</FormLabel>
                 <div className = "d-flex gap-2">
                     <input className = "form-control form-control-sm"
                         name = 'firstName'
@@ -61,8 +129,12 @@ const Register:React.FunctionComponent<IRegisterProps> = (props) => {
                         placeholder = "ConfirmPassword"
                         onChange = { onChange }
                     />
-                    <Button className = "btn btn-primary btn-sm w-50">Register</Button>
+                    <Button className = "btn btn-primary btn-sm w-50"
+                        onClick = { regiterHandler }
+                    >Register</Button>
             </div>
+                </div>
+            ) }
         </form>
     )
 }
